824a08180a4761149f108fec9d11b609f00809b3
Sasha <mix.filipov@mail.ru>
2019-09-23 11:31:13 -0700

    authorization is ready for use

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # spider-back-end
 This is the back-end repository for the Build Sprint Project: Spider Graph
+
+### User Schema
+
+Users in the database conform to the following object structure:
+
+```js
+{
+  username: "name", // String, required
+  password: "password",  // String, required
+}
+```
+
+###Endpoints for the user
+
+| Method  |      URL           |     Description                |
+| ------- | ------------------ |------------------------------- | 
+| POST    | /api/auth/register | Creates a new user and returns an id of newly created user |
+| POST    | /api/auth/login    | Returns the token for the user |
+
---
+++ routers/auth/authRouter.js
@@ -0,0 +1,49 @@
+const db = require('./authDB');
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secret = require('../../config/secret');
+
+router.post('/register', (req, res) => {
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+    console.log('Hi I am here ',req.body);
+    if (user) {
+        db.add(user)
+            .then(user => res.status(201).json(user[0]))
+            .catch(err => res.status(500).json({ error: "There was an error while saving the user to the database" }))
+    } else {
+        res.status(401).json({errorMessage: 'Invalid Credentials'});
+    }
+});
+
+router.post('/login', (req, res) => {
+    const {username, password} = req.body;
+    if (username && password) {
+        db.findBy({username})
+            .first()
+            .then(user => {
+                if(user && bcrypt.compareSync(password, user.password)){
+                    const token = generateToken(user);
+                    res.status(200).json(token);
+                }
+            })
+    } else {
+        res.status(401).json({errorMessage: "Invalid Credentials"})
+    }
+});
+
+function generateToken(user){
+    const payload = {
+        subject:user.id,
+        username:user.username
+    };
+    const options ={
+        expiresIn: '1d'
+    };
+
+    return jwt.sign(payload, secret.jwtSecret, options);
+}
+
+module.exports = router;
