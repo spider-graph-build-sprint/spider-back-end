9c75210e3da8d243b005c34b39f54d28ecc0d8c0
Sasha <mix.filipov@mail.ru>
2019-09-26 12:04:23 -0700

    nodoc

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,4 +1,6 @@
 define({
+  "title": "spider-graph",
+  "url": "https://spider-back-end.herokuapp.com",
   "name": "spider-back-end",
   "version": "1.0.0",
   "description": "This is the back-end repository for the Build Sprint Project: Spider Graph",
@@ -7,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-09-26T17:16:04.700Z",
+    "time": "2019-09-26T17:57:33.834Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
